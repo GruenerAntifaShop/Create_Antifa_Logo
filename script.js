2f3213ac5552eb4baaae06e0d0207a92bbcb4d3a
Martin Antrag <Martin@gruenerAntifaShop.de>
2022-07-20 16:31:48 +0200

    add extended Text Change

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var JSON_IMAGES = {}
 var JSON_CONFIG = {}
 var NEW_IMAGES = 0
-var position_struc = ["height", "width", "top", "left", "flip"]
+var POSITION_IMAGE = ["height", "width", "top", "left", "flip"]
+var POSITION_TEXT = ["text", "size", "style", "top", "left", "bend", "flip", "color"]
 document.getElementById("colorpicker_outer_circle").value = "#000000"
 document.getElementById("colorpicker_inner_circle").value = "#ffffff"
+
+
+/*
 document.getElementById("colorpicker_text").value = "#ffffff"
 document.getElementById("input_upper_text").value = "ANTIFASCHISTISCHE"
 document.getElementById("input_down_text").value = "AKTION"
@@ -14,12 +18,12 @@ circleType.radius(245).dir(1);
 circleType = new CircleType(document.getElementById('down_text'));
 circleType.radius(240).dir(-1);
 
-
+*/
 document.getElementById('image_picker').onchange = function(evt) {
   new_image = {
     "name": "",
     "files": [],
-    "position": []
+    "files_position": []
   }
   if (window.File && window.FileList && window.FileReader) {
     var files = evt.target.files;
@@ -31,7 +35,7 @@ document.getElementById('image_picker').onchange = function(evt) {
       picReader.addEventListener("load", function(event) {
         var picFile = event.target;
         new_image["files"].push(picFile.result)
-        new_image["position"].push([200, 230, 240, 65, 1])
+        new_image["files_position"].push([200, 230, 240, 65, 1])
       });
       //Read the image
       picReader.readAsDataURL(files[i]);
@@ -43,8 +47,6 @@ document.getElementById('image_picker').onchange = function(evt) {
   }
 }
 
-//  console.log(document.getElementById("upper_text").setAttribute("x" ,250-(document.getElementById("upper_text").getComputedTextLength()/2)))
-//  console.log(document.getElementById("down_text").setAttribute("x" ,250-(document.getElementById("down_text").getComputedTextLength()/2)))
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -53,9 +55,12 @@ xhttp.onreadystatechange = function() {
     //console.log(xhttp.responseText);
     JSON_IMAGES = JSON.parse(xhttp.responseText)["images"]
     JSON_CONFIG = JSON.parse(xhttp.responseText)["config"]
-    load_images()
+    for (JSON_IMAGE_ID in JSON_IMAGES) {
+      //console.log(value)
+      load_mini_images(JSON_IMAGE_ID)
+    }
     add_event_lister()
-    set_data_image(JSON_IMAGES[JSON_CONFIG["first"]])
+    set_data_image(JSON_CONFIG["first"])
   } else if (this.status == 404) {
     console.log(this.responseURL)
   }
@@ -64,113 +69,178 @@ xhttp.open("GET", "./images/images.json", true);
 xhttp.withCredentials = true;
 xhttp.send();
 
-function load_images() {
-  console.log(JSON_IMAGES)
-  document.getElementById("mini_images_field").innerHTML = ""
-  for (value in JSON_IMAGES) {
-    //console.log(json[value])
+function load_mini_images(JSON_IMAGE_ID) {
+  data = JSON_IMAGES[JSON_IMAGE_ID]
+  if (document.getElementById("mini_images_div" + JSON_IMAGE_ID) == undefined) {
     elm = document.createElement("div")
     elm.classList.add("mini_images_div")
-    for (image_id in JSON_IMAGES[value]["files"]) {
-      image = document.createElement("img")
-      image.src = JSON_IMAGES[value]["files"][image_id]
-      image.classList.add("mini_images")
-      image.style.height = JSON_IMAGES[value]["position"][image_id][0] / 8 + "px"
-      image.style.width = JSON_IMAGES[value]["position"][image_id][1] / 8 + "px"
-      image.style.top = JSON_IMAGES[value]["position"][image_id][2] / 8 + "px"
-      image.style.left = JSON_IMAGES[value]["position"][image_id][3] / 8 + "px"
-      image.style.transform = "scaleX(" + JSON_IMAGES[value]["position"][image_id][4] + ")"
-      image.style.position = "absolute"
-      elm.appendChild(image)
-    }
-    elm.addEventListener("click", function(value) {
-      return function() {
-        //console.log(value)
-        set_data_image(JSON_IMAGES[value])
-      }
-    }(value))
-
-    document.getElementById("mini_images_field").appendChild(elm)
+    elm.id = "mini_images_div" + JSON_IMAGE_ID
+  } else {
+    document.getElementById("mini_images_div" + JSON_IMAGE_ID).innerHTML = ""
+    elm = document.getElementById("mini_images_div" + JSON_IMAGE_ID)
   }
+  //console.log(json[value])
+  for (image_id in data["files"]) {
+    image = document.createElement("img")
+    image.src = data["files"][image_id]
+    image.classList.add("mini_images")
+    image.style.height = data["files_position"][image_id][0] / 8 + "px"
+    image.style.width = data["files_position"][image_id][1] / 8 + "px"
+    image.style.top = data["files_position"][image_id][2] / 8 + "px"
+    image.style.left = data["files_position"][image_id][3] / 8 + "px"
+    image.style.transform = "scaleX(" + data["files_position"][image_id][4] + ")"
+    image.style.position = "absolute"
+    elm.appendChild(image)
+  }
+  elm.addEventListener("click", function(JSON_IMAGE_ID) {
+    return function() {
+      //console.log(value)
+      set_data_image(JSON_IMAGE_ID)
+    }
+  }(JSON_IMAGE_ID))
+
+  document.getElementById("mini_images_field").appendChild(elm)
+
 }
 
-function set_data_image(data) {
-  console.log(data)
+function set_data_image(JSON_IMAGE_ID) {
+  //console.log(JSON_IMAGE_ID)
+  data = JSON_IMAGES[JSON_IMAGE_ID]
+  //console.log(data)
   document.getElementById("circle_images").innerHTML = ""
   document.getElementById("controll_image_size").innerHTML = ""
-  for (image_id in data["files"]) {
 
-    //console.log(data["position"][image_id])
+
+  for (image_id in data["files"]) {
+    //console.log(data["files_position"][image_id])
     image = document.createElement("img")
     image.src = data["files"][image_id]
     image.classList.add("max_images")
-    image.style.height = data["position"][image_id][0] + "px"
-    image.style.width = data["position"][image_id][1] + "px"
-    image.style.top = data["position"][image_id][2] + "px"
-    image.style.left = data["position"][image_id][3] + "px"
-    image.style.transform = "scaleX(" + data["position"][image_id][4] + ")"
+    image.style.height = data["files_position"][image_id][0] + "px"
+    image.style.width = data["files_position"][image_id][1] + "px"
+    image.style.top = data["files_position"][image_id][2] + "px"
+    image.style.left = data["files_position"][image_id][3] + "px"
+    image.style.transform = "scaleX(" + data["files_position"][image_id][4] + ")"
     //image.classList.add("")
     document.getElementById("circle_images").appendChild(image)
-    for (position in position_struc) {
-      //console.log(position_struc[position])
-
+    for (position in POSITION_IMAGE) {
+      //console.log(POSITION_IMAGE[position])
       label = document.createElement("label")
-      label.innerHTML = position_struc[position] + " " + image_id
+      label.innerHTML = POSITION_IMAGE[position] + " " + image_id
       input = document.createElement("input")
-      input.id = position_struc[position] + image_id
-      if (position_struc[position] == "flip") {
+      input.id = POSITION_IMAGE[position] + image_id
+      if (POSITION_IMAGE[position] == "flip") {
         input.type = "checkbox"
-        if (data["position"][image_id][position] == "-1") {
+        if (data["files_position"][image_id][position] == "-1") {
           input.setAttribute("checked", "true")
         }
-      } else {
-        input.type = "number"
-
       }
-      input.setAttribute("value", data["position"][image_id][position])
+      input.setAttribute("value", data["files_position"][image_id][position])
+
       label.appendChild(input)
       document.getElementById("controll_image_size").appendChild(label)
     }
-
-
     document.getElementById("controll_image_size").innerHTML += "<br>"
   }
 
+  //addEventListener
   for (image_id in data["files"]) {
-    position_struc[position]
-    for (position in position_struc) {
-      //console.log(position_struc[position] + image_id)
-      document.getElementById(position_struc[position] + image_id).addEventListener("change", function(data, image_id, position) {
+    for (position in POSITION_IMAGE) {
+      document.getElementById(POSITION_IMAGE[position] + image_id).addEventListener("change", function(JSON_IMAGE_ID, image_id, position) {
         return function() {
-          if (position_struc[position] == "flip") {
-            console.log("test")
-            if (document.getElementById(position_struc[position] + image_id).value == "1") {
-              document.getElementById(position_struc[position] + image_id).value = "-1"
+          input = document.getElementById(POSITION_IMAGE[position] + image_id)
+          //console.log(POSITION_IMAGE[position])
+          if (POSITION_IMAGE[position] == "flip") {
+            if (input.value == "1") {
+              input.value = "-1"
             } else {
-              document.getElementById(position_struc[position] + image_id).value = "1"
+              input.value = "1"
             }
           }
-          data["position"][image_id][position] = document.getElementById(position_struc[position] + image_id).value
-          console.log(data)
-          load_images()
-          set_data_image(data)
+          JSON_IMAGES[JSON_IMAGE_ID]["files_position"][image_id][position] = input.value
+          //console.log(JSON_IMAGE_ID)
+          load_mini_images(JSON_IMAGE_ID)
+          set_data_image(JSON_IMAGE_ID)
         }
-      }(data, image_id, position))
+      }(JSON_IMAGE_ID, image_id, position))
     }
   }
+  //LOAD TEXT IN BIG Image
+
+  document.getElementById("image_field_text").innerHTML = ""
+  document.getElementById("text_inputs").innerHTML = ""
+  for (text_id in data["text"]) {
+    //console.log(data["text"][text_id][0])
+    //console.log(data["text"][text_id])
+    text_div = document.createElement("div")
+
+
+    text_div.innerHTML = data["text"][text_id][0]
+    text_div.id = "image_field_text" + text_id
+    text_div.classList.add("text_circle")
+    text_div.style.fontSize = data["text"][text_id][1] + "px"
+    text_div.style.fontWeight = data["text"][text_id][2]
+    text_div.style.top = data["text"][text_id][3] + "px"
+    text_div.style.left = data["text"][text_id][4] + "px"
+    text_div.style.color = data["text"][text_id][7]
+    document.getElementById("image_field_text").appendChild(text_div)
+
+    circleType = new CircleType(document.getElementById("image_field_text" + text_id));
+    circleType.radius(data["text"][text_id][5]).dir(data["text"][text_id][6]);
+
+    input_div = document.createElement("div")
+    label = document.createElement("label")
+
+    //document.getElementById("text_inputs").innerHTML += "<br>"
+
+    for (value in data["text"][text_id]) {
+      label = document.createElement("label")
+      label.innerHTML = POSITION_TEXT[value]
+      input = document.createElement("input")
+      input.id = "image_field_text" + POSITION_TEXT[value] + text_id
+      if (POSITION_TEXT[value] == "color") {
+        input.type = "color"
+      }
+      input.setAttribute("value", data["text"][text_id][value])
+      label.appendChild(input)
+      input_div.appendChild(label)
+    }
+
+
+    document.getElementById("text_inputs").appendChild(input_div)
+
+    /*<input type = "text"id = "input_upper_text"value = "ANTIFASCHISTISCHE" > < input type = "color" id = "colorpicker_text" value = "#ffffff" > < br >*/
+  }
+
+  for (text_id in data["text"]) {
+    for (position in POSITION_TEXT) {
+      //console.log(POSITION_TEXT[position] + text_id)
+      document.getElementById("image_field_text" + POSITION_TEXT[position] + text_id).addEventListener("change", function(JSON_IMAGE_ID, text_id, position) {
+        return function() {
+          input = document.getElementById("image_field_text" + POSITION_TEXT[position] + text_id)
+          console.log(POSITION_TEXT[position], text_id, input.value)
+          if (POSITION_TEXT[position] == "flip") {
+            if (input.value == "1") {
+              input.value = "-1"
+            } else {
+              input.value = "1"
+            }
+          }
+          JSON_IMAGES[JSON_IMAGE_ID]["text"][text_id][position] = input.value
+          //console.log(JSON_IMAGE_ID)
+          load_mini_images(JSON_IMAGE_ID)
+          set_data_image(JSON_IMAGE_ID)
+        }
+      }(JSON_IMAGE_ID, text_id, position))
+
+    }
+  }
+
 }
 
+
+
 function add_event_lister() {
-  document.getElementById("input_upper_text").addEventListener("input", function() {
-    document.getElementById("upper_text").innerHTML = document.getElementById("input_upper_text").value
-    circleType = new CircleType(document.getElementById('upper_text'));
-    circleType.radius(245).dir(1);
-  });
-  document.getElementById("input_down_text").addEventListener("input", function() {
-    document.getElementById("down_text").innerHTML = document.getElementById("input_down_text").value
-    circleType = new CircleType(document.getElementById('down_text'));
-    circleType.radius(240).dir(-1);
-  });
   document.getElementById("colorpicker_outer_circle").addEventListener("input", function() {
     console.log(document.getElementById("colorpicker_outer_circle").value)
     document.getElementById("circle_out").style.background = document.getElementById("colorpicker_outer_circle").value
@@ -179,11 +249,7 @@ function add_event_lister() {
     console.log(document.getElementById("colorpicker_inner_circle").value)
     document.getElementById("circle_in").style.background = document.getElementById("colorpicker_inner_circle").value
   });
-  document.getElementById("colorpicker_text").addEventListener("input", function() {
-    console.log(document.getElementById("colorpicker_text").value)
-    document.getElementById("upper_text").style.color = document.getElementById("colorpicker_text").value
-    document.getElementById("down_text").style.color = document.getElementById("colorpicker_text").value
-  });
+
   /*
   document.getElementById("reload_button").addEventListener("click", function() {
     load_images()
